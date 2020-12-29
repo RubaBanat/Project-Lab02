6b315d2b3f35492bb7a6e1eec4eeb823bb273cb0
rubabanat <rubabanat1996@gmail.com>
2020-12-29 09:54:39 +0200

    update

--- JS/app.js
+++ JS/app.js
@@ -2,12 +2,12 @@
 
 var userInput = prompt('What is your name?'); //to entract with the user
 alert('Hello '+userInput);
-console.log (userInput);
+//console.log (userInput);
 
 
 
  var user = prompt('How are you feeling today? (good/bad)');
- console.log(user);
+ //console.log(user);
  console.log(user.toLowerCase());
 
  if (user.toLowerCase() === 'good') {
@@ -16,40 +16,44 @@ console.log (userInput);
     alert ('Never mind Im sure you will be ok');
   }
 
-  var first = confirm ('We will play guessing me game are you excited?'); // only takes boolean values (true/ false)
-console.log(first);
- if(first){
+  var first = confirm ('We will play guessing me game with a random general Quwstions ..are you excited?' ); // only takes boolean values (true/ false)
+ //console.log(first);
+ if(first == true){
      alert('Great Lets start :)');
  } else {
      alert('oh no :( Im sure you will have fun so come on!');
  }
 
- var usergame = prompt('What is my Favorite color? choose one (Red. Black, Yellow)');
+ var usergame = prompt('Is my Favorite color black ? choose  (Yes/No)'); //start with the first question
+  //console.log(usergame);
  console.log(usergame.toLowerCase());
- if (usergame === 'black'){
+ if (usergame == 'yes'){
 alert (' Right black is my favorite :D lets go to the next Question ');
  } else {
      alert ('No you are wrong black is my favorite :( lets go to the next Question ');
  }
  
- var usergame1 = prompt('How old am I ? choose one (twenty four , twenty five, twenty seven)');
+ var usergame1 = prompt('am I 24 Years Old ? choose  (Yes/No)');
+  //console.log(usergame1);
  console.log(usergame1.toLowerCase());
- if (usergame1 === 'twenty four'){
+ if (usergame1 === 'yes'){
 alert (' Right Im 24 yers old :D lets go to the next Question ');
  } else {
      alert ('No you are wrong Im 24 years old :( lets go to the next Question ');
  }
 
   
- var usergame2 = prompt('Do I think before I act ? choose one (No not Really, Defiantly, Most of the time)');
+ var usergame2 = prompt('Do I think before I act ? choose one (Yes/No)');
+  //console.log(usergame2);
  console.log(usergame2.toLowerCase());
- if (usergame2 === 'most of the time'){
+ if (usergame2 === 'yes'){
 alert (' Yes right :D lets go to the next Question ');
  } else {
      alert ('No you are wrong I think before I act most of the time  :( lets go to the next Question ');
  }
 
- var usergame3 = prompt('Do I like horror movies? ? choose one (Yes / No)');
+ var usergame3 = prompt('Do I like horror movies? ? choose one (Yes / No)'); // last question
+  //console.log(usergame3);
  console.log(usergame3.toLowerCase());
  if (usergame3 === 'yes'){
 alert (' Yes right :D ');
@@ -63,10 +67,10 @@ alert (' Yes right :D ');
  switch (theLastQ) {
 
  case 'yes':
- alert ('That is great have fun in my webpage');
+ alert ('That is great ' +userInput + ' have fun in my webpage');
  break;
  case 'no':
- alert ('sorry for bothering you :((');
+ alert ('sorry for bothering you '+ userInput + ' Hope you enjoy my webpage' );
  break;
  
 
